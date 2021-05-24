6b994c6ccf9303efa5a5be512c669d1535f51c24
jforjay1 <jaypatel8914@gmail.com>
2021-05-24 10:39:21 +0530

    virtualendcommit

--- weekdistrict.py
+++ weekdistrict.py
@@ -9,7 +9,7 @@ from telethon.tl.types import InputPeerUser, InputPeerChannel
 from telethon import TelegramClient, sync, events
 
 header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36'}
-district = [770,160]
+district = [770,160,179]
 final_message = []
 knownpincodes = []
 knowndates = []
@@ -22,8 +22,8 @@ token = '1813049791:AAEQwqA1AsRHofLLIp0zW9bDVcw1mikVewQ'
 phone = '+917016062635'
 
 
-x = datetime.datetime.now()
-myTuple = (x.strftime("%d"),x.strftime("%m"),x.strftime("%Y"))
+xtime = datetime.datetime.now()
+myTuple = (xtime.strftime("%d"),xtime.strftime("%m"),xtime.strftime("%Y"))
 loopstatus = False
 looptrack = 1
 while not loopstatus:
@@ -38,15 +38,15 @@ while not loopstatus:
                     ses = x['sessions']
                     fee_type = x['fee_type']
                     for s in ses:
-                        if ((x['pincode'] in knownpincodes) and (x['date'] in knowndates)):
+                        if ((x['pincode'] in knownpincodes) and (s['date'] in knowndates)):
                             skippincode = x['pincode']
-                            skipdate = x['date']
-                        if ((s['available_capacity_dose1']>0) and (fee_type=='Free') and (s['min_age_limit']==45) and (skippincode!=x['pincode']) and (skipdate!=x['date'])):
+                            skipdate = str(s['date'])
+                        if ((s['available_capacity_dose1']>0) and (fee_type=='Free') and (s['min_age_limit']==45) and ((skippincode!=x['pincode']) or (skipdate!=s['date']))):
                             message = "\n For Pincode "+str(x['pincode'])+"("+str(x['district_name'])+") and date "+str(s['date'])+" total availablility of age group above 45 is "+str(s['available_capacity'])+"\n Dose 1 are "+str(s['available_capacity_dose1'])+" and Dose 2 are "+str(s['available_capacity_dose2'])
                             final_message.append(message)
                             pin = x['pincode']
                             knownpincodes.append(pin)
-                            d = x['date']
+                            d = s['date']
                             knowndates.append(d)
                             #loopstatus = True
                             #print(loopstatus)
@@ -57,9 +57,9 @@ while not loopstatus:
                 pass
         except KeyError:
             pass
-    print(looptrack,"times")
+    print(looptrack,"times", xtime)
     looptrack = looptrack + 1
-    time.sleep(60)
+    time.sleep(80)
 
     #telegram code
 
